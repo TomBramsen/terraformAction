dfa2e58a060bd0cd91e2a87d78d5b2854baf8fd0
Tom <skodbjarne@gmail.com>
2023-01-17 06:55:50 -0800

    first commit

--- backend.tf
+++ backend.tf
@@ -7,9 +7,9 @@ terraform {
   }
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state-001"
-    storage_account_name = "cloudninjaterraformstate"
-    container_name       = "tfstate"
-    key                  = "GitHub-Terraform-rg-connectivity-001"
+   // storage_account_name = "cloudninjaterraformstate"
+   // container_name       = "tfstate"
+   //  key                  = "GitHub-Terraform-rg-connectivity-001"
   }
 }
 provider "azurerm" {
